18fe36e3070d781f69201180ba2edc2c56846904
Ivy Liu <ivyliu402@gmail.com>
2019-04-12 17:30:24 +0200

    firstday

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function main(){
+
+  const mainElement= document.querySelector('main');
+    function buildDom(html){
+        mainElement.innerHTML = html;
+        return mainElement;
+    }
+
+
---
+++ read_me.js
@@ -0,0 +1,54 @@
+# Project's name
+
+云端Fun
+
+## Description
+
+Brief description of the project
+This is a game for Chinese word review. 
+The purpose of this game is for you to reinforce the Chinese characters after
+a lesson with a teacher for a new word introduction.
+
+## MVP (DOM - CANVAS)
+MVP definition, deliverables.
+CANVAS
+
+## Backlog
+* one player
+* A moving Clouds platform
+
+
+## Data structure
+Classes and methods definition.
+
+
+
+## States y States Transitions
+Definition of the different states and their transition (transition functions)
+
+- splashScreen
+- gameScreen
+- gameoverScreen
+- winScreen
+
+
+## Task
+Task definition in order of priority
+
+
+## Links
+
+
+### Trello
+[Link url](https://trello.com)
+
+
+### Git
+URls for the project repo and deploy
+[Link Repo](http://github.com)
+[Link Deploy](http://github.com)
+
+
+### Slides
+URls for the project presentation (slides)
+[Link Slides.com](http://slides.com)
