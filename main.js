c172a7f6d3aecf99522fdd17287291308a7759c0
Ivy Liu <ivyliu402@gmail.com>
2019-04-13 20:02:35 +0200

    README done

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 'use strict';
 
 function main(){
-
   const mainElement= document.querySelector('main');
     function buildDom(html){
         mainElement.innerHTML = html;
         return mainElement;
-    }
+    };
 
 
--- read_me.js
+++ read_me.js
@@ -14,26 +14,120 @@ MVP definition, deliverables.
 CANVAS
 
 ## Backlog
-* one player
-* A moving Clouds platform
+* Words in each cloud
+* Randomly generate word on slides
+* Characters
+* Sounds
+* Beautify
 
 
 ## Data structure
-Classes and methods definition.
+game.js
 
+function Game(canvas){
+}
 
+Game.prototype.startLoop=function(){
+  loop()
+}
+
+Game.prototype.updateCanvas=function(){
+}
+
+Game.prototype.drawCanvas=function(){
+}
+
+Game.prototype.clearCanavas=function(){
+
+}
+
+Game.prototype.checkAllCollisons=function(){
+}
+
+Game.prototype.finishGameCallback=function(){
+}
+
+player.js
+
+function Player(canvas){
+    this.lives 
+    this.size
+    this.canvas
+    this.x
+    this.y
+    this.speed
+}
+
+player.prototype.drawCanvas=function(){
+
+}
+player.prototype.updateCanvas=function(){
+
+}
+player.prototype.setDirectionCanvas=function(){
+
+}
+player.prototype.setLvesCanvas=function(){
+
+}
+
+player.prototype.checkcollisionWithClouds=function(){
+
+}
+
+clouds.js
+
+function Clouds(Canvas.y){
+    this.speed
+    this.size
+    this.direction
+    this.canvas
+    this.ctx
+    this.x
+    this.y
+}
+
+Clouds.prototype.drawCanvas=function(){
+
+}
+
+clouds.prototype.updateCanvas=function(){
+
+}
 
 ## States y States Transitions
 Definition of the different states and their transition (transition functions)
 
 - splashScreen
+	-buildSplash
+	-addEventListener(startGame)
+
 - gameScreen
+	-creat new Game()
+	-game.start();
+
 - gameoverScreen
-- winScreen
+	- restartScreen()
+	- addEventListener()
 
 
 ## Task
 Task definition in order of priority
+Main - buildDom
+Main - buildSplash
+Main - addEventListener
+Game - buildDom
+Game - restartGame
+Game - addEventListener
+Block - create
+Game - create player
+Player - create
+Player - move
+Player - gravity
+Player - collision
+Player - jump
+Clouds - moving direction
+
 
 
 ## Links
